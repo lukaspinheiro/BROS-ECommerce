7f9751df27529fcfe5cec40aa945a24aaa263b9a
Lukas Pinheiro <lukas.pinheiro123@gmail.com>
2025-05-18 23:32:50 -0400

    [TSK]BROS-22-criar-tela-de-visualizacao-do-produto [WIP] Tentando fazer visualização dinamica para mais de um tipo de produto com uma view

---
+++ BROS-ECommerce.Services/Services/ServiceProduto.cs
@@ -0,0 +1,41 @@
+﻿using BROS_ECommerce.Services.Interface.Services;
+using BROS_ECommerce.Services.ViewModel.Produto;
+
+namespace BROS_ECommerce.Services.Services
+{
+    public class ProdutoService : IServiceProduto
+    {
+        private static List<ProdutoViewModel> _produtos = new List<ProdutoViewModel>
+        {
+            new ProdutoViewModel
+            {
+                Id = Guid.NewGuid(),
+                Nome = "Whey Protein",
+                Slug = "whey-protein",
+                Descricao = "Suplemento de proteína ideal para ganho de massa muscular.",
+                ImagemUrl = "/imagens/whey.jpg",
+                Preco = 149.99M
+            },
+            new ProdutoViewModel
+            {
+                Id = Guid.NewGuid(),
+                Nome = "Creatina",
+                Slug = "creatina",
+                Descricao = "Melhora o desempenho físico em exercícios repetidos de curta duração e alta intensidade.",
+                ImagemUrl = "/imagens/creatina.jpg",
+                Preco = 149.99M
+            }
+        };
+
+        public List<ProdutoViewModel> ObterTodos()
+        {
+            return _produtos;
+        }
+
+        public ProdutoViewModel? ObterPorSlug(string slug)
+        {
+            return _produtos.FirstOrDefault(p => p.Slug.Equals(slug, StringComparison.OrdinalIgnoreCase));
+        }
+    }
+
+}
---
+++ BROS-ECommerce.Web/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,16 @@
+﻿using BROS_ECommerce.Services.Interface.Services;
+using BROS_ECommerce.Services.Services;
+
+namespace BROS_ECommerce.Web.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+        public static void RegisterServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            #region Serviços
+            services.AddScoped<IServiceProduto, ProdutoService>();
+
+            #endregion
+        }
+    }
+}
