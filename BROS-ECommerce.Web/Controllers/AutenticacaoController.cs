3fa9f085d3bab208add13e6c819b5ad2be655db9
Luis Marcelo <lmfg10@gmail.com>
2025-06-09 09:42:01 -0400

    [TSK] BROS-25-criar-tela-de-login [IMP] Criação da tela de login

---
+++ BROS-ECommerce.Web/Controllers/AutenticacaoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class AutenticacaoController : Controller
+{
+    [HttpGet]
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Login(string usuario, string senha)
+    {
+        
+        return RedirectToAction("Index", "Home");
+    }
+}
