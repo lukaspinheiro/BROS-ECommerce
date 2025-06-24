d519deef03994aeb7fe4723131378624f296529c
Daniel-Vinicius1 <musycgame1@gmail.com>
2025-06-24 15:55:36 -0400

    [TSK]BROS-31-criar-banco-de-dados-para-registro-de-usuarios [IMP]

---
+++ BROS-ECommerce.Domain/Entities/User.cs
@@ -0,0 +1,60 @@
+﻿
+
+using BROS_ECommerce.Domain.Interfaces;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BROS_ECommerce.Domain.Entities
+{
+    public class User : IAggregateRoot
+    {
+        public User() { }
+
+        [Key]
+        public Guid IdUser { get; set; } = Guid.NewGuid();
+
+        [Required]
+        [StringLength(255)]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(14)] 
+        public string Cpf { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(200)]
+        public string Nome { get; set; } = string.Empty;
+
+        [Required]
+        [Column(TypeName = "date")]
+        public DateTime Nascimento { get; set; }
+
+        [Required]
+        [StringLength(500)] 
+        public string Senha { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(20)]
+        public string Genero { get; set; } = string.Empty;
+
+        [Column(TypeName = "datetime2")]
+        public DateTime DataCriacao { get; set; } = DateTime.UtcNow;
+
+        [Column(TypeName = "datetime2")]
+        public DateTime? DataAtualizacao { get; set; }
+
+        public bool Ativo { get; set; } = true;
+
+       
+        [NotMapped]
+        public int Idade => DateTime.Now.Year - Nascimento.Year -
+            (DateTime.Now.DayOfYear < Nascimento.DayOfYear ? 1 : 0);
+
+        [NotMapped]
+        public string CpfFormatado =>
+            Cpf.Length == 11 ?
+                $"{Cpf.Substring(0, 3)}.{Cpf.Substring(3, 3)}.{Cpf.Substring(6, 3)}-{Cpf.Substring(9, 2)}" :
+                Cpf;
+    }
+}
--- BROS-ECommerce.Infra/Context/BrosContext.cs
+++ BROS-ECommerce.Infra/Context/BrosContext.cs
@@ -1,14 +1,11 @@
-﻿using BROS_ECommerce.Domain.Entities;
+﻿
+
+using BROS_ECommerce.Domain.Entities;
 using BROS_ECommerce.Domain.Interfaces.Crud;
 using BROS_ECommerce.Infra.EntityConfig;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BROS_ECommerce.Infra.Context
 {
@@ -23,10 +20,16 @@ namespace BROS_ECommerce.Infra.Context
 
         
         public DbSet<Produto> Produtos { get; set; }
+        public DbSet<User> Users { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            
             modelBuilder.ApplyConfiguration(new ProdutoMap());
+            modelBuilder.ApplyConfiguration(new UserMap());
+
+            
+            SeedUsers(modelBuilder);
 
             base.OnModelCreating(modelBuilder);
         }
@@ -41,6 +44,61 @@ namespace BROS_ECommerce.Infra.Context
         public async Task<bool> SaveChangesAsync()
         {
             return await base.SaveChangesAsync() > 0;
+        }
+
+        
+        private void SeedUsers(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasData(
+                new User
+                {
+                    IdUser = new Guid("11111111-1111-1111-1111-111111111111"),
+                    Email = "admin@bros.com",
+                    Cpf = "12345678901",
+                    Nome = "Administrador Sistema",
+                    Nascimento = new DateTime(1990, 1, 1),
+                    Senha = "AQAAAAEAACcQAAAAEJ8D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D", 
+                    Genero = "Masculino",
+                    DataCriacao = new DateTime(2025, 1, 1, 0, 0, 0, DateTimeKind.Utc), 
+                    Ativo = true
+                },
+                new User
+                {
+                    IdUser = new Guid("22222222-2222-2222-2222-222222222222"),
+                    Email = "joao.silva@gmail.com",
+                    Cpf = "98765432100",
+                    Nome = "João Silva Santos",
+                    Nascimento = new DateTime(1995, 5, 15),
+                    Senha = "AQAAAAEAACcQAAAAEJ8D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D", 
+                    Genero = "Masculino",
+                    DataCriacao = new DateTime(2025, 1, 1, 0, 0, 0, DateTimeKind.Utc), 
+                    Ativo = true
+                },
+                new User
+                {
+                    IdUser = new Guid("33333333-3333-3333-3333-333333333333"),
+                    Email = "maria.oliveira@hotmail.com",
+                    Cpf = "45678912300",
+                    Nome = "Maria Oliveira Costa",
+                    Nascimento = new DateTime(1998, 8, 22),
+                    Senha = "AQAAAAEAACcQAAAAEJ8D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D",
+                    Genero = "Feminino",
+                    DataCriacao = new DateTime(2025, 1, 1, 0, 0, 0, DateTimeKind.Utc), 
+                    Ativo = true
+                },
+                new User
+                {
+                    IdUser = new Guid("44444444-4444-4444-4444-444444444444"),
+                    Email = "alex.santos@outlook.com",
+                    Cpf = "78912345600",
+                    Nome = "Alex Santos Lima",
+                    Nascimento = new DateTime(2000, 12, 10),
+                    Senha = "AQAAAAEAACcQAAAAEJ8D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D8Q9D", 
+                    Genero = "Outro",
+                    DataCriacao = new DateTime(2025, 1, 1, 0, 0, 0, DateTimeKind.Utc), 
+                    Ativo = true
+                }
+            );
         }
     }
 }
