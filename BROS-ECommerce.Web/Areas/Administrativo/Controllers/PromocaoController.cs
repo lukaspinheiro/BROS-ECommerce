7474a7fca02d29647d1146cc7a6ad45d98ba6475
Luis Marcelo <lmfg10@gmail.com>
2025-07-17 13:25:56 -0400

    [TSK] BROS-92-realizar-crud-e-logica-de-gerenciar-promocoes [WIP] Controller de promoções

---
+++ BROS-ECommerce.Web/Areas/Administrativo/Controllers/PromocaoController.cs
@@ -0,0 +1,57 @@
+﻿using BROS_ECommerce.Services.Interface.Services;
+using BROS_ECommerce.Services.ViewModel.Home;
+using BROS_ECommerce.Services.ViewModel.Promocao;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BROS_ECommerce.Web.Areas.Administrativo.Controllers
+{
+    [Area("Administrativo")]
+    [Route("Administrativo/Promocao")]
+    public class PromocaoController : Controller
+    {
+        private readonly IServicePromocao _servicePromocao;
+        private readonly IServiceProduto _serviceProduto;
+
+        public PromocaoController(IServicePromocao servicePromocao, IServiceProduto serviceProduto)
+        {
+            _servicePromocao = servicePromocao;
+            _serviceProduto = serviceProduto;
+        }
+
+        [HttpGet("Index")]
+        public async Task<IActionResult> Index()
+        {
+            var promocoes = await _servicePromocao.ObterAtivosParaHomeAsync();
+            var viewModel = new HomeViewModel
+            {
+                Promocoes = promocoes
+            };
+            return View(viewModel);
+        }
+
+
+        [HttpPost("Cadastrar")]
+        public async Task<IActionResult> Cadastrar(CadastrarPromocaoViewModel vm)
+        {
+            await _servicePromocao.AdicionarAsync(vm);
+            TempData["Sucesso"] = "Promoção cadastrada com sucesso!";
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost("Atualizar")]
+        public async Task<IActionResult> Atualizar(CadastrarPromocaoViewModel vm)
+        {
+            await _servicePromocao.AtualizarAsync(vm);
+            TempData["Sucesso"] = "Promoção atualizada com sucesso!";
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost("Excluir/{id}")]
+        public async Task<IActionResult> Excluir(Guid id)
+        {
+            await _servicePromocao.ExcluirAsync(id);
+            TempData["Sucesso"] = "Promoção excluída!";
+            return RedirectToAction("Index");
+        }
+    }
+}
