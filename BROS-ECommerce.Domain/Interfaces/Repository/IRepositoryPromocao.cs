2e54ead9f945adf9a930930b69be2040698f26f6
Luis Marcelo <lmfg10@gmail.com>
2025-07-17 13:24:42 -0400

    [TSK] BROS-92-realizar-crud-e-logica-de-gerenciar-promocoes [WIP] Repositorio de promoções

---
+++ BROS-ECommerce.Domain/Interfaces/Repository/IRepositoryPromocao.cs
@@ -0,0 +1,14 @@
+﻿using BROS_ECommerce.Domain.Entities;
+
+namespace BROS_ECommerce.Domain.Interfaces.Repository
+{
+    public interface IRepositoryPromocao
+    {
+        Task<IEnumerable<Promocao>> ObterTodasComProdutoAsync();
+        Task<IEnumerable<Promocao>> ObterAtivasAsync();
+        Task<Promocao?> ObterPorIdAsync(Guid id);
+        Task AdicionarAsync(Promocao promocao);
+        Task AtualizarAsync(Promocao promocao);
+        Task ExcluirAsync(Guid id);
+    }
+}
