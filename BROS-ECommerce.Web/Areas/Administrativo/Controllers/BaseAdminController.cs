78465795070d4d4161f17a9f8b59561945e1c74f
Luis Marcelo <lmfg10@gmail.com>
2025-07-17 09:32:52 -0400

    [TSK] BROS-91-criar-controller-para-garantir-que-o-usuario-comum-nao-consiga-acessar-a-pagina-administrativa [WIP] Criei controller para garantir que o usuario comum nao consiga entrar pela rota na página administrativa

---
+++ BROS-ECommerce.Web/Areas/Administrativo/Controllers/BaseAdminController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace BROS_ECommerce.Web.Areas.Administrativo.Controllers
+{
+    [Area("Administrativo")]
+    [Authorize]
+    public abstract class BaseAdminController : Controller
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var email = User.Identity?.Name;
+            var emailAdmin = "admin@gymbros.com";
+
+            if (email != emailAdmin)
+            {
+                context.Result = RedirectToAction("Login", "Autenticacao", new { area = "" });
+                return;
+            }
+
+            base.OnActionExecuting(context);
+        }
+    }
+}
