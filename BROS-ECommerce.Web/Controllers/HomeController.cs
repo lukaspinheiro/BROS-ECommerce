b44164baa7ca7fabd2fd143ca3295cd0c5daf4bd
Luis Marcelo <lmfg10@gmail.com>
2025-07-17 13:26:13 -0400

    [TSK] BROS-92-realizar-crud-e-logica-de-gerenciar-promocoes [WIP] Editando HomeController

--- BROS-ECommerce.Web/Controllers/HomeController.cs
+++ BROS-ECommerce.Web/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using BROS_ECommerce.Services.Interface.Services;
 using BROS_ECommerce.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -27,6 +28,19 @@ namespace BROS_ECommerce.Web.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private readonly IServicePromocao _servicePromocao;
+
+        public HomeController(IServicePromocao servicePromocao)
+        {
+            _servicePromocao = servicePromocao;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var promocoes = await _servicePromocao.ObterAtivosParaHomeAsync();
+            return View(promocoes); // você pode criar uma ViewModel mais completa se quiser
         }
     }
 }
