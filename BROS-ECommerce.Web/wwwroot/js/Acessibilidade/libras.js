fba443815f746fbd157b6046b5119ff052a9a85b
LuisMFG <lmfg10@gmail.com>
2025-07-17 02:41:23 -0400

    [TSK] BROS-69-inserir-a-api-v-libras [WIP] Scripts da API de libras inseridas

---
+++ BROS-ECommerce.Web/wwwroot/js/Acessibilidade/libras.js
@@ -0,0 +1,9 @@
+﻿document.getElementById('libras').style.display = 'none';
+$('#btnlibras').on('click', function () {
+    if (document.getElementById('libras').style.display == 'none') {
+        document.getElementById('libras').style.display = 'block';
+    }
+    else {
+        document.getElementById('libras').style.display = 'none';
+    }
+});
