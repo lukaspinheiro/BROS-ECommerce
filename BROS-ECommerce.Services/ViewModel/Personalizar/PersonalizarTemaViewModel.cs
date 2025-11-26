e57c8b678ac4507403d7a1935a1684d4cbf2badb
Lukas Pinheiro <lukas.pinheiro123@gmail.com>
2025-11-25 22:41:27 -0400

    [TSK]BROS-94-como-desenvolvedor-preciso-refatorar-o-sistema-para-um-multi-tenant [WIP]módulo para alterar tema e logo do tenant

---
+++ BROS-ECommerce.Services/ViewModel/Personalizar/PersonalizarTemaViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BROS_ECommerce.Services.ViewModel.Personalizar;
+
+public class PersonalizarLojaViewModel
+{
+    public string? CorMenu { get; set; }
+    public string? CorTextoMenu { get; set; }
+
+    public string? CorFundo { get; set; }
+    public string? CorTexto { get; set; }
+
+    public string? CorMenuInferior { get; set; }
+    public string? CorTextoMenuInferior { get; set; }
+
+    public IFormFile? LogoArquivo { get; set; }
+}
+
