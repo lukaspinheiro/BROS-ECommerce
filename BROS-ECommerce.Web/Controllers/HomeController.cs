9b2c6c0eee7a3e47e3973ef4be1b705d8e5e969f
Lukas Pinheiro <lukas.pinheiro123@gmail.com>
2025-11-27 01:23:37 -0400

    [TSK]BROS-94-como-desenvolvedor-preciso-refatorar-o-sistema-para-um-multi-tenant [WIP]pequenas correções

--- BROS-ECommerce.Web/Controllers/HomeController.cs
+++ BROS-ECommerce.Web/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using BROS_ECommerce.Services.Interface.Services;
 using BROS_ECommerce.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -7,15 +8,18 @@ namespace BROS_ECommerce.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IServiceProduto _serviceProduto;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IServiceProduto serviceProduto)
         {
             _logger = logger;
+            _serviceProduto = serviceProduto;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var produtos = await _serviceProduto.ObterTabelaProdutosAsync();
+            return View(produtos);
         }
 
         public IActionResult Privacy()
